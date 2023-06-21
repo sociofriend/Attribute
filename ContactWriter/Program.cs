667d1ed2dcecc4b0d5c27fd3625521a658a0f680
sociofriend <lilit_avdalyan@yahoo.com>
2023-06-21 22:22:56 +0400

    Initial commit

---
+++ ContactWriter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AttributesProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contact sarah = new Contact()
+        {
+            FirstName = "Sarah Conner",
+            AgeInYears = 38
+        };
+
+        var sarahWriter = new ContactConsoleWriter(sarah);
+        sarahWriter.Write();
+
+        Console.WriteLine("\n\nHit enter to exit...");
+        Console.ReadLine();
+    }
+}
+
